ed3078ec2ce07b7ba999613ca79bde04da642056
fatemeHbstar <165134410+fatemeHbstar@users.noreply.github.com>
2025-11-06 07:43:47 +0330

    Update exp_short_term_forecasting.py
    Changed loss a little bit

--- exp/exp_short_term_forecasting.py
+++ exp/exp_short_term_forecasting.py
@@ -99,7 +99,12 @@ class Exp_Short_Term_Forecast(Exp_Basic):
                 batch_y = batch_y[:, -self.args.pred_len:, f_dim:].to(self.device)
 
                 batch_y_mark = batch_y_mark[:, -self.args.pred_len:, f_dim:].to(self.device)
-                loss_value = criterion(batch_x, self.args.frequency_map, outputs, batch_y, batch_y_mark)
+                if isinstance(criterion, nn.MSELoss):
+                    loss_value = criterion(outputs, batch_y)
+                else:
+                    # Assume other loss functions (MAPE, MASE) need the extra arguments
+                    loss_value = criterion(batch_x, self.args.frequency_map, outputs, batch_y, batch_y_mark)
+                #loss_value = criterion(batch_x, self.args.frequency_map, outputs, batch_y, batch_y_mark)
                 loss_sharpness = mse((outputs[:, 1:, :] - outputs[:, :-1, :]), (batch_y[:, 1:, :] - batch_y[:, :-1, :]))
                 loss = loss_value  # + loss_sharpness * 1e-5
                 train_loss.append(loss.item())
