dd21f439c7a5e9519f982b2a21d72b39f2671fe9
fatemeHbstar <165134410+fatemeHbstar@users.noreply.github.com>
2025-11-07 15:52:29 +0330

    Add CustomSegLoader support in data_factory.py

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,5 +1,5 @@
 from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, PSMSegLoader, \
-    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader
+    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader, CustomSegLoader
 from data_provider.uea import collate_fn
 from torch.utils.data import DataLoader
 
@@ -10,6 +10,7 @@ data_dict = {
     'ETTm2': Dataset_ETT_minute,
     'custom': Dataset_Custom,
     'm4': Dataset_M4,
+    'CustomSegLoader': CustomSegLoader,
     'PSM': PSMSegLoader,
     'MSL': MSLSegLoader,
     'SMAP': SMAPSegLoader,
@@ -30,19 +31,31 @@ def data_provider(args, flag):
 
     if args.task_name == 'anomaly_detection':
         drop_last = False
-        data_set = Data(
-            args = args,
-            root_path=args.root_path,
-            win_size=args.seq_len,
-            flag=flag,
-        )
+        if args.data == 'CustomSegLoader':
+            data_set = Data(
+                root_path=args.root_path,
+                data_path=args.data_path,
+                win_size=args.seq_len,
+                step=1,
+                flag=flag,
+                scale=True
+            )
+        else:
+            data_set = Data(
+                args=args,
+                root_path=args.root_path,
+                win_size=args.seq_len,
+                flag=flag,
+            )
+
         print(flag, len(data_set))
         data_loader = DataLoader(
             data_set,
             batch_size=batch_size,
             shuffle=shuffle_flag,
             num_workers=args.num_workers,
-            drop_last=drop_last)
+            drop_last=drop_last
+        )
         return data_set, data_loader
     elif args.task_name == 'classification':
         drop_last = False
